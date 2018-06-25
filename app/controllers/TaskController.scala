4ea1cc3c65375b12a8620d740be5db6918045c61
Kolade <koladeadewuyi@gmail.com>
2018-06-25 20:35:15 +0100

    Add TaskController

---
+++ app/controllers/TaskController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+@Singleton
+class TaskController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: String) = TODO
+
+}
